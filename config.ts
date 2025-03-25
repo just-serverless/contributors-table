3058fa24df8542e08539b2efd8fef50b701ec435
Okinea Dev <hi@okinea.dev>
2025-03-25 08:05:31 +0100

    chore: update useragent

--- config.ts
+++ config.ts
@@ -2,5 +2,4 @@
  * Application authentication that is presented
  * with each request to the GitHub API
  */
-// TODO: Find out if it is necessary
-export const USER_AGENT = 'Contributors Table App by @okineadev (unpublished)'
+export const USER_AGENT = 'Contributors Table App by @okineadev'
